8bd1e60309c89ddfec69e2fa1a85c21732a49713
Gael <gaelfranciscoM.47@gmail.com>
2024-04-30 10:58:36 -0600

    Correccion desktopMenu y carritoMenuIcon

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenuIcon = document.querySelector('.menu');
+const carritoMenuIcon = document.querySelector('.navbar-shopping-cart');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenuIcon.addEventListener('click', toggleMobileMenu);
+carritoMenuIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {//esta funcion y toggleCarritosAside funciona para quitar mobileMenu u viceversa aside para que no se encimen
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');   
+    }
+
+    mobileMenu.classList.toggle('inactive'); //inactive(html) funciona para que el menu al iniciar la pag este oculto hasta dar el click
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
